5bed8dea1efbbeb1791765c853d2060c7c38b3ad
naimdeb <naimdebard02@gmail.com>
2025-03-25 16:11:20 +0100

    added get companies command

--- src/Service/ExternalApiService.php
+++ src/Service/ExternalApiService.php
@@ -9,6 +9,7 @@ class ExternalApiService
 {
     private $client;
     private $params;
+    private const IGDB_API_URL = 'https://api.igdb.com/v4';
 
     public function __construct(HttpClientInterface $client, ParameterBagInterface $params)
     {
@@ -28,69 +29,112 @@ class ExternalApiService
     }
 
 
+    private function getIgdbHeaders(): array
+    {
+        return [
+            'Authorization' => 'Bearer ' . $this->params->get('IGDB_API_KEY'),
+            'Client-ID' => $this->params->get('IGDB_CLIENT_ID'),
+            'Content-Type' => 'application/json',
+            'Accept' => '*/*'
+        ];
+    }
+
+    private function makeIgdbRequest(string $endpoint, string $body)
+    {
+        $response = $this->client->request('POST', self::IGDB_API_URL . $endpoint, [
+            'headers' => $this->getIgdbHeaders(),
+            'body' => $body,
+        ]);
+
+        return $response;
+    }
+
+    private function getCount(string $endpoint, string $whereClause = ''): int
+    {
+        $body = "fields id;\n" . $whereClause . "\nlimit 1;";
+        $response = $this->makeIgdbRequest($endpoint, $body);
+        return (int) $response->getHeaders()['x-count'][0];
+    }
+
     public function getIgdbGames(int $limit, int $offset = 0)
     {
-    
-        $body = '
-        fields id, name, platforms.*, involved_companies.company.name, first_release_date, genres.id, cover.url;
-        where game_type = 0;
-        limit ' . $limit . ';
-        offset ' . $offset . ';';
-
-
-        $response = $this->client->request('POST', 'https://api.igdb.com/v4/games', [
-            'headers' => [
-                'Authorization' => 'Bearer ' . $this->params->get('IGDB_API_KEY'),
-                'Client-ID' => $this->params->get('IGDB_CLIENT_ID'),
-                'Content-Type' => 'application/json',
-                'Accept' => '*/*'
-            ],
-            'body' => $body,
-
+        $body = $this->buildQueryBody([
+            'fields' => 'id, name, platforms.*, involved_companies.company.name, first_release_date, genres.id, cover.url',
+            'where' => 'game_type = 0',
+            'limit' => $limit,
+            'offset' => $offset
         ]);
 
-        $content = $response->getContent();
-
-        // todo : Error handling
-
-
-        return json_decode($content, true);
-        
-        // $contentType = $response->getHeaders()['content-type'][0];
-
-
-        // return $response->then(
-        //     function ($response) {
-        //         return $response->toArray(); // Handle the successful response
-        //     },
-        //     function ($error) {
-        //         // Handle any errors
-        //         throw new \Exception('The IGDB /games API request with a limit of ' . $limit . ' and an offset of ' . $offset . '  failed: ' . $error->getMessage());
-        //     }
-        // );
+        $response = $this->makeIgdbRequest('/games', $body);
+        return json_decode($response->getContent(), true);
     }
 
-    public function getNumberOfIgdbGames()
+    public function getIgdbExtensions(int $limit, int $offset = 0)
     {
-
-        $body = '
-        fields id, name, platforms.*, involved_companies.company.name, first_release_date, genres.id, artworks.url;
-        where game_type = 0;
-        limit 1;';
-
-        $response = $this->client->request('POST', 'https://api.igdb.com/v4/games', [
-            'headers' => [
-                'Authorization' => 'Bearer ' . $this->params->get('IGDB_API_KEY'),
-                'Client-ID' => $this->params->get('IGDB_CLIENT_ID'),
-                'Content-Type' => 'application/json',
-                'Accept' => '*/*'
-            ],
-            'body' => $body,
-
+        $body = $this->buildQueryBody([
+            'fields' => 'id, name, platforms.*, involved_companies.company.name, first_release_date, genres.id, cover.url',
+            'where' => 'game_type = (1,2,4,6,7)',
+            'limit' => $limit,
+            'offset' => $offset
         ]);
 
-        $contentType = $response->getHeaders()['x-count'][0];
-        return $contentType;
-
+        $response = $this->makeIgdbRequest('/games', $body);
+        return json_decode($response->getContent(), true);
     }
+
+    public function getIgdbGenres(int $limit, int $offset = 0)
+    {
+        $body = $this->buildQueryBody([
+            'fields' => 'id, name',
+            'limit' => $limit,
+            'offset' => $offset
+        ]);
+
+        $response = $this->makeIgdbRequest('/genres', $body);
+        return json_decode($response->getContent(), true);
+    }
+
+    public function getIgdbCompanies(int $limit, int $offset = 0)
+    {
+        $body = $this->buildQueryBody([
+            'fields' => 'id, name',
+            'limit' => $limit,
+            'offset' => $offset
+        ]);
+
+        $response = $this->makeIgdbRequest('/companies', $body);
+        return json_decode($response->getContent(), true);
+    }
+
+    private function buildQueryBody(array $params): string
+    {
+        $query = '';
+        if (isset($params['fields'])) $query .= "fields {$params['fields']};\n";
+        if (isset($params['where'])) $query .= "where {$params['where']};\n";
+        if (isset($params['limit'])) $query .= "limit {$params['limit']};\n";
+        if (isset($params['offset'])) $query .= "offset {$params['offset']};";
+        return $query;
+    }
+
+    public function getNumberOfIgdbGames(): int
+    {
+        return $this->getCount('/games', 'where game_type = 0');
+    }
+
+    public function getNumberOfIgdbExtensions(): int
+    {
+        return $this->getCount('/games', 'where game_type = (1,2,4,6,7)');
+    }
+
+    public function getNumberOfIgdbGenres(): int
+    {
+        return $this->getCount('/genres');
+    }
+
+    public function getNumberOfIgdbCompanies(): int
+    {
+        return $this->getCount('/companies');
+    }
+
+    
 }
