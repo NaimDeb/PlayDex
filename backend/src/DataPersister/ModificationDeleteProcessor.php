fc74dd0a3d972df217ed71831f404c04d7e0b783
Naïm D. <130140746+NaimDeb@users.noreply.github.com>
2025-06-20 16:07:57 +0200

    Added a WarningService to add a Warning  when deleted by an admin

--- backend/src/DataPersister/ModificationDeleteProcessor.php
+++ backend/src/DataPersister/ModificationDeleteProcessor.php
@@ -5,14 +5,19 @@ namespace App\DataPersister;
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProcessorInterface;
 use App\Entity\Modification;
+use App\Entity\Patchnote;
 use App\Entity\Report;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use App\Service\WarningService;
 
 class ModificationDeleteProcessor implements ProcessorInterface
 {
     public function __construct(
-        private EntityManagerInterface $entityManager
+        private EntityManagerInterface $entityManager,
+        private WarningService $warningService,
+        private Security $security 
     ) {}
 
     public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = []): void
@@ -45,5 +50,16 @@ class ModificationDeleteProcessor implements ProcessorInterface
         // Save all changes
         $this->entityManager->persist($data);
         $this->entityManager->flush();
+
+        $author = $data->getUser(); 
+        $admin = $this->security->getUser();
+
+        if ($author && $author !== $admin) {
+            $this->warningService->warnUserForDeletion(
+                target: $author,
+                admin: $admin,
+                reason: sprintf('Votre %s a été supprimé par un administrateur.', $data instanceof Patchnote ? 'patchnote' : 'modification')
+            );
+        }
     }
 }
