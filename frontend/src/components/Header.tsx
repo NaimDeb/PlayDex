0e41da25b2bb2f41bef64cf2753d5b9bb8b71492
naimdeb <naimdebard02@gmail.com>
2025-07-07 11:18:58 +0200

    Changed addToast to showMessage (also tailwind extension to cleanup the classes)

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import Link from "next/link";
 import { Logo } from "./Logo";
 import { useAuth } from "@/providers/AuthProvider";
+import { useFlashMessage } from "@/components/FlashMessage/FlashMessageProvider";
 import {
   SearchIcon,
   XIcon,
@@ -21,6 +22,8 @@ export function Header() {
   const { user, isAuthenticated, logout } = useAuth();
   const [searchOpen, setSearchOpen] = useState(false);
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false); // Nouvel état pour le menu mobile
+  const { showMessage } = useFlashMessage();
+  
 
   // Fonctions pour basculer l'état et fermer l'autre panneau
   const toggleSearch = () => {
@@ -117,7 +120,7 @@ export function Header() {
           isMobile ? "w-full mt-2" : "shrink-0"
         }`} // shrink-0 pour desktop
       >
-        <SearchIcon className="h-6 w-6 mx-auto" />
+        <SearchIcon className="w-6 h-6 mx-auto" />
       </button>
     </form>
   );
@@ -125,7 +128,7 @@ export function Header() {
   return (
     // Fragment pour permettre le menu déroulant comme élément adjacent au header
     <>
-      <header className="relative h-16 flex w-full overflow-hidden bg-secondary text-white">
+      <header className="relative flex w-full h-16 overflow-hidden text-white bg-secondary">
         {/* 1. Logo (toujours visible) */}
         <div className="bg-[#18181b] h-full flex items-center px-2 z-30 relative shrink-0">
           {/* Assure-toi que Logo est un lien vers l'accueil */}
@@ -136,24 +139,24 @@ export function Header() {
 
         {/* 2. Triangle gauche */}
         <div
-          className="h-full w-8 md:w-16 z-20 flex-shrink-0 bg-off-black"
+          className="z-20 flex-shrink-0 w-8 h-full md:w-16 bg-off-black"
           style={{ clipPath: "polygon(0 0, 0 100%, 100% 100%)" }}
         />
 
         {/* 3. Bande centrale avec Nav (Bureau seulement) */}
-        <div className="flex-1 h-full bg-secondary items-center justify-center relative z-10 overflow-hidden hidden md:flex">
+        <div className="relative z-10 items-center justify-center flex-1 hidden h-full overflow-hidden bg-secondary md:flex">
           {/* Nav bureau (cachée si recherche ouverte) */}
           {!searchOpen && (
-            <nav className="font-bold text-xl">
-              <Link href="/" className="hover:text-gray-200 px-2">
+            <nav className="text-xl font-bold">
+              <Link href="/" className="px-2 hover:text-gray-200">
                 Accueil
               </Link>
-              <Link href="/#jeux" className="hover:text-gray-200 px-2">
+              <Link href="/#jeux" className="px-2 hover:text-gray-200">
                 Jeux
               </Link>
               <a
                 onClick={handleRandomGame}
-                className="hover:text-gray-200 px-2 cursor-pointer"
+                className="px-2 cursor-pointer hover:text-gray-200"
               >
                 Jeu au hasard
               </a>
@@ -168,7 +171,7 @@ export function Header() {
         >
           {/* Formulaire de recherche Bureau (dans le triangle) */}
           {searchOpen && (
-            <div className="hidden md:block w-full h-full">
+            <div className="hidden w-full h-full md:block">
               {" "}
               {/* Conteneur pour positionner le form */}
               <SearchFormComponent isMobile={false} />
@@ -179,19 +182,19 @@ export function Header() {
         {/* 5. Section droite avec icônes/boutons */}
         <div className="bg-[#18181b] h-full flex justify-end items-center px-4 space-x-4 z-30 ml-auto max-md:w-[80vw] md:ml-0 shrink-0">
           {/* Icônes/Boutons Bureau */}
-          <div className="hidden md:flex items-center space-x-4">
+          <div className="items-center hidden space-x-4 md:flex">
             <button className="hover:text-gray-200" onClick={toggleSearch}>
               {searchOpen ? (
-                <XIcon className="h-6 w-6" />
+                <XIcon className="w-6 h-6" />
               ) : (
-                <SearchIcon className="h-6 w-6" />
+                <SearchIcon className="w-6 h-6" />
               )}
             </button>
             {isAuthenticated ? (
               <div className="flex items-center space-x-4">
                 <Link
                   href="/profile"
-                  className="hover:text-gray-200 flex items-center space-x-2"
+                  className="flex items-center space-x-2 hover:text-gray-200"
                 >
                   <UserCircleIcon className="h-7 w-7" />
                   <span className="text-lg font-bold">{user?.username}</span>
@@ -208,7 +211,7 @@ export function Header() {
                   className="hover:text-red-400"
                   title="Déconnexion"
                 >
-                  <LogoutIcon className="h-6 w-6" />
+                  <LogoutIcon className="w-6 h-6" />
                 </button>
               </div>
             ) : (
@@ -218,7 +221,7 @@ export function Header() {
                 </a>
                 <a
                   href="/register"
-                  className="bg-indigo-500 hover:bg-indigo-600 text-white py-1 px-3 rounded"
+                  className="px-3 py-1 text-white bg-indigo-500 rounded hover:bg-indigo-600"
                 >
                   Inscription
                 </a>
@@ -227,12 +230,12 @@ export function Header() {
           </div>
 
           {/* Icônes Mobile */}
-          <div className="flex md:hidden items-center space-x-4 px-8">
+          <div className="flex items-center px-8 space-x-4 md:hidden">
             <button className="hover:text-gray-200" onClick={toggleSearch}>
               {searchOpen ? (
-                <XIcon className="h-6 w-6" />
+                <XIcon className="w-6 h-6" />
               ) : (
-                <SearchIcon className="h-6 w-6" />
+                <SearchIcon className="w-6 h-6" />
               )}
             </button>
             {isAuthenticated ? (
@@ -241,14 +244,14 @@ export function Header() {
               </Link>
             ) : (
               <a href="/login" className="hover:text-gray-200">
-                <LoginIcon className="h-6 w-6" />
+                <LoginIcon className="w-6 h-6" />
               </a>
             )}
             <button className="hover:text-gray-200" onClick={toggleMobileMenu}>
               {mobileMenuOpen ? (
-                <XIcon className="h-6 w-6" />
+                <XIcon className="w-6 h-6" />
               ) : (
-                <MenuIcon className="h-6 w-6" />
+                <MenuIcon className="w-6 h-6" />
               )}
             </button>
           </div>
@@ -261,67 +264,65 @@ export function Header() {
         {/* md:hidden pour ne l'afficher que sur mobile */}
         {/* Dropdown Recherche Mobile */}
         {searchOpen && (
-          <div className="absolute top-0 left-0 right-0 bg-gray-800 p-4 shadow-lg border-t border-gray-700">
+          <div className="absolute top-0 left-0 right-0 p-4 bg-gray-800 border-t border-gray-700 shadow-lg">
             <SearchFormComponent isMobile={true} />
           </div>
         )}
         {/* Dropdown Menu Mobile */}
         {mobileMenuOpen && (
-          <div className="absolute top-0 left-0 right-0 bg-gray-800 p-4 shadow-lg border-t border-gray-700">
+          <div className="absolute top-0 left-0 right-0 p-4 bg-gray-800 border-t border-gray-700 shadow-lg">
             <nav className="flex flex-col space-y-4 text-lg font-medium">
               <Link
                 href="/"
                 onClick={() => setMobileMenuOpen(false)}
-                className="hover:text-gray-200 px-2 py-1"
+                className="px-2 py-1 hover:text-gray-200"
               >
                 Accueil
               </Link>
               <Link
                 href="/#jeux"
                 onClick={() => setMobileMenuOpen(false)}
-                className="hover:text-gray-200 px-2 py-1"
+                className="px-2 py-1 hover:text-gray-200"
               >
                 Jeux
               </Link>
               <a
                 onClick={handleRandomGame}
-                className="hover:text-gray-200 px-2 py-1 cursor-pointer"
+                className="px-2 py-1 cursor-pointer hover:text-gray-200"
               >
                 Jeu au hasard
               </a>
               {/* Liens spécifiques utilisateur si connecté */}
               {isAuthenticated && (
                 <>
-                  <hr className="border-gray-600 my-2" />
+                  <hr className="my-2 border-gray-600" />
                   <a
                     href="/ma-liste"
                     onClick={() => setMobileMenuOpen(false)}
-                    className="flex items-center space-x-2 hover:text-gray-200 px-2 py-1"
+                    className="flex items-center px-2 py-1 space-x-2 hover:text-gray-200"
                   >
-                    <ClipboardListIcon className="h-6 w-6" />
+                    <ClipboardListIcon className="w-6 h-6" />
                     <span>Ma liste</span>
                   </a>
                   <Link
                     href="/profile"
                     onClick={() => setMobileMenuOpen(false)}
-                    className="flex items-center space-x-2 hover:text-gray-200 px-2 py-1"
+                    className="flex items-center px-2 py-1 space-x-2 hover:text-gray-200"
                   >
-                    <UserCircleIcon className="h-6 w-6" />
+                    <UserCircleIcon className="w-6 h-6" />
                     <span>Profil ({user?.username})</span>
                   </Link>
                   <button
                     onClick={() => {
                       if (logout) logout();
                       setMobileMenuOpen(false);
-                      addToast({
-                        title: "Déconnexion réussie",
-                        description: "Vous avez été déconnecté avec succès.",
-                        severity: "success",
-                      });
+                      showMessage(
+                        "Vous avez été déconnecté avec succès.", "success"
+                      );
                     }}
-                    className="flex items-center space-x-2 text-red-400 hover:text-red-300 px-2 py-1 text-left"
+                    className="flex items-center px-2 py-1 space-x-2 text-left text-red-400 hover:text-red-300"
                   >
-                    <LogoutIcon className="h-6 w-6" />
+                    <LogoutIcon className="w-6 h-6" />
                     <span>Déconnexion</span>
                   </button>
                 </>
@@ -329,18 +330,18 @@ export function Header() {
               {/* Liens Connexion/Inscription si non connecté */}
               {!isAuthenticated && (
                 <>
-                  <hr className="border-gray-600 my-2" />
+                  <hr className="my-2 border-gray-600" />
                   <a
                     href="/login"
                     onClick={() => setMobileMenuOpen(false)}
-                    className="hover:text-gray-200 px-2 py-1"
+                    className="px-2 py-1 hover:text-gray-200"
                   >
                     Connexion
                   </a>
                   <a
                     href="/register"
                     onClick={() => setMobileMenuOpen(false)}
-                    className="bg-indigo-500 hover:bg-indigo-600 text-white py-1 px-3 rounded text-center mx-2"
+                    className="px-3 py-1 mx-2 text-center text-white bg-indigo-500 rounded hover:bg-indigo-600"
                   >
                     Inscription
                   </a>
