f5d43a679fccb517e114150a5cddda60a0e1f59e
Naïm D. <130140746+NaimDeb@users.noreply.github.com>
2025-07-09 10:08:39 +0200

    image change

--- frontend/src/components/Logo.tsx
+++ frontend/src/components/Logo.tsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export function Logo({width = 100, height = 100}: {width?: number; height?: number}) {
   return (
-    <Image src="/logo.png" alt="Logo" width={width} height={height} style={{ objectFit: 'cover' }} />
+    <Image src="/logo.webp" alt="Logo" width={width} height={height} style={{ objectFit: 'cover' }} />
   );
 }
