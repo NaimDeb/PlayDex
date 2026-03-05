55665a9b84d703e89bc9eb989b8ebd81d2cb3302
Naïm D. <naimdebard02@gmail.com>
2026-03-05 10:23:04 +0100

    remove : Removed search by follows as the filter has been deprecated
    feat : added a filter to themes to remove erotic games from the fetched games

--- backend/src/Config/ApiConfig.php
+++ backend/src/Config/ApiConfig.php
@@ -11,8 +11,8 @@ final readonly class ApiConfig {
     public const IGDB_BATCH_SIZE = 500;
     public const IGDB_PARALLEL_REQUESTS = 4;
     public const IGDB_RATE_LIMIT_DELAY_US = 250000; // Microsecondes
-    public const IGDB_GAME_MIN_FOLLOWS = 5;
     public const IGDB_SEARCH_CACHE_TTL = 14400; // 4 hours in seconds
+    public const FORBIDDEN_THEMES = 42; // Erotic games
 
 // PHP Memory
 
